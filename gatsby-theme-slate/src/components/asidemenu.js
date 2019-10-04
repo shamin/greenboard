d1917011d93a66ebe2196ce4d670266d109a57fd
Shamin <shamin616@gmail.com>
2019-10-05 00:57:37 +0530

    Scrollspy for sidebar links

--- gatsby-theme-slate/src/components/asidemenu.js
+++ gatsby-theme-slate/src/components/asidemenu.js
@@ -4,6 +4,48 @@ import _ from 'lodash'
 export default class AsideMenu extends Component {
   constructor(props) {
     super(props)
+
+    this.scrollSpy = this.scrollSpy.bind(this);
+    this.scrollSpyOffset = 300;
+
+    this.state = {
+      activeNavItem: false,
+    };
+  }
+
+  componentDidMount() {
+    this.scrollSpy();
+    window.addEventListener('scroll', this.scrollSpy);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scrollSpy);
+  }
+
+  scrollSpy() {
+    const selector = 'h1, h2';
+    const headers = Array.from(document.querySelectorAll(selector));
+
+    const headersOffset = headers.map((el, i) => {
+      const slug = el.textContent;
+      let nextEl;
+
+      if ((headers.length - 1) !== i) {
+        nextEl = headers[i + 1].getBoundingClientRect().top;
+      } else {
+        nextEl = headers[i].getBoundingClientRect().top + 9000;
+      }
+
+      const offset = (nextEl + document.body.scrollTop) - this.scrollSpyOffset;
+
+
+      return { slug, offset };
+    });
+
+
+    const headersScrollTop = headersOffset.filter(el => el.offset > 0);
+
+    this.setState({ activeNavItem: headersScrollTop[0] });
   }
 
   getLinks(docs) {
@@ -44,20 +86,22 @@ export default class AsideMenu extends Component {
     return merged
   }
 
-
+  isChild(parent, slug) {
+    return (parent.children.findIndex((e) => e.textNode === slug) >= 0)
+  }
 
   render() {
     const { docs } = this.props
-    // parseHeadings(docs)
+    const { activeNavItem } = this.state
     const links = this.getLinks(docs)
     return (
       <React.Fragment>
         <ul className="toc-list-h1">
           {links.map((linkh1) =>
-            <li>
-              <a key={linkh1.textNode} className="toc-h1 toc-link">{linkh1.textNode}</a>
-              <ul class="toc-list-h2 active" style={{display: "block"}}>
-                {linkh1.children.map((linkh2) => <a key={linkh2.textNode} className="toc-h2 toc-link">{linkh2.textNode}</a>)}
+            <li key={linkh1.textNode}>
+              <a className={`toc-h1 toc-link ${activeNavItem.slug === linkh1.textNode ? "active" : ""}`}>{linkh1.textNode}</a>
+              <ul className={`toc-list-h2 active ${this.isChild(linkh1, activeNavItem.slug) ? "active-parent" : ""}`} style={{ display: this.isChild(linkh1, activeNavItem.slug) ? "block" : "none" }}>
+                {linkh1.children.map((linkh2) => <a key={linkh2.textNode} className={`toc-h2 toc-link ${activeNavItem.slug === linkh2.textNode ? "active" : ""}`}>{linkh2.textNode}</a>)}
               </ul>
             </li>
           )}
